4945ca43a8570d80bb93675076bd962226ecb7b0
harii84 <99866727+harii84@users.noreply.github.com>
2023-05-19 22:58:00 +0530

    uni

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import PreFooter from "@/components/home/PreFooter";
 import Hero from "@/components/home/hero/Hero";
 import ShowDown from "@/components/home/showdown/ShowDown";
-import MouseTrail from "@pjsalita/react-mouse-trail";
 import Head from "next/head";
 
 export default function Home() {
@@ -27,7 +26,7 @@ export default function Home() {
         <PreFooter />
       </main>
       <div className="hidden sm:block">
-        <MouseTrail {...config} />
+       
       </div>
     </>
   );
